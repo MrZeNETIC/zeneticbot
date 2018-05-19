6592e2b542cf12f9ba4b89b38d64e124c576e718
MrZeNETIC <39440623+MrZeNETIC@users.noreply.github.com>
2018-05-19 16:33:32 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,24 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'please') {
+    	message.reply('no');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'memes pls') {
+    	message.reply('A link cuz idk how to image, https://www.google.ca/search?q=ting+meme&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiyqfOM2JLbAhUrrlQKHS6eDtUQ_AUICigB&biw=1920&bih=974#imgrc=iBr3usWYc4rIsM:');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '!help') {
+    	message.reply('This bot is still under development, however available commands inclue the folllowing:');
+        message.channel.sendMessage(' !help: list commands \n memes pls: gives a link to a meme \n ping: replies with pong');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
